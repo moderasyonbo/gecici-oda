a6edffd0e1b07236860b5cb9d547c260452790b2
moderasyonbo <93944142+moderasyonbo@users.noreply.github.com>
2022-06-07 21:39:50 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  setInterval(function() {}, 8000);
+ 
+  console.log(`Giriş Başarılı!`);
+};
